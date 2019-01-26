c4c10e4d9393fba5a6c566ac133ed625d229d355
Yonatahn Elbaz <yonielbz@gmail.com>
2019-01-26 14:17:20 +0200

    Created UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Text title;
+    public Text end;
+
+    public enum MenuType
+    {
+        Start,
+        End,
+    }
+
+    public void Init(MenuType type)
+    {
+        title.gameObject.SetActive(type == MenuType.Start);
+        end.gameObject.SetActive(type == MenuType.End);
+    }
+
+    public void StartGame()
+    {
+        GameManager.instance.StartGame();
+    }
+
+    public void Credits()
+    {
+       
+    }
+}
