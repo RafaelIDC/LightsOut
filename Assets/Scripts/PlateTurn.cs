7a3f33587c357540a6085f40904a30b32276d6fe
Yonatahn Elbaz <yonielbz@gmail.com>
2019-01-26 02:31:22 +0200

    Working micro and fixed ramen orefab

---
+++ Assets/Scripts/PlateTurn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateTurn : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
